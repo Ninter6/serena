09af8f0db3f80fdd8c515a296d035512e46b7ce5
Ninter6 <mc525740@outlook.com>
2024-09-01 16:13:03 +0800

    first commit

---
+++ src/core.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ninter6 on 2024/9/1.
+//
+
+#include "core.hpp"
---
+++ src/core.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Ninter6 on 2024/9/1.
+//
+
+#ifndef SERENA_CORE_HPP
+#define SERENA_CORE_HPP
+
+#endif //SERENA_CORE_HPP
